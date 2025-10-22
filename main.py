f6140bf64ac5f20146dd6912dd1805efa3719eee
松坂眠花糖 <563424794@qq.com>
2025-10-22 19:35:04 +0800

    使用SSE模式

--- main.py
+++ main.py
@@ -2,37 +2,50 @@ import os
 import time
 
 import flask
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, Response
 
 import socket
 from threading import Thread
 import threading
 
+from flask_sslify import SSLify
+
 import data
 import sync
 from data import load_meta_data
 import broadcast_listener
-
 app = Flask(__name__)
 
 @app.route('/api/data/pull', methods=['GET'])
-def pull_data():
+def get_data():
     print("收到拉取数据")
-    return jsonify(sync.pull_data())
+    return jsonify(sync.get_data())
 
-@app.route('/api/data/push', methods=['POST'])
-def push_data():
+@app.route('/api/data/push', methods=['PUT'])
+def put_data():
     print("收到同步数据")
     data = request.get_json()
-    print(f"收到数据: {data}")  # 在实际应用中，这里可以处理数据
-    res = sync.push_data(data)
-    # 怎么广播到其他在线客户端?
+    print(f"收到数据: {data}")
+    res = sync.put_data(data)
     print(f"返回数据: {res}")
     return jsonify(res)
 
 @app.route('/api/toast', methods=['GET'])
 def send_toast():
     return jsonify({})
+
+def sync_time_stream():
+    while True:
+        yield sync.get_todo_data().get_sync_time()
+        time.sleep(1)
+
+@app.route('/sync_time', methods=['GET'])
+def sync_time():
+    return Response(
+        sync_time_stream(),
+        mimetype="text/event-stream",
+        headers={'Cache-Control': 'no-cache'}
+    )
 
 if __name__ == '__main__':
     hello_port = data.get_hello_port()
--- sync.py
+++ sync.py
@@ -11,20 +11,12 @@ class SYNC_RESULT(int, Enum):
 def pack_result(result: SYNC_RESULT, sync_time:str, tab_list:list) -> dict:
     return {"result":result,"sync_time":sync_time,"tab_list":tab_list}
 
-def pull_data() -> dict:
+def get_data() -> dict:
     server_data = get_todo_data()
     return pack_result(SYNC_RESULT.SUCCESS, server_data.get_sync_time(), server_data.get_tab_list())
 
-def push_data(client_data) -> dict:
+def put_data(client_data) -> dict:
     server_data = get_todo_data()
-
-    # 如果客户端不一致, 则返回新数据, 要求客户端保持同步, 其实也可以要求客户端自行pull
-    if client_data["sync_time"] != server_data.get_sync_time():
-        print("时间不一致",client_data["sync_time"], "!=", server_data.get_sync_time())
-        return pack_result(SYNC_RESULT.FAIL, server_data.get_sync_time(), server_data.get_tab_list())
-
     # 更新到客户端的数据, 同时更新时间点
-    print("时间一致")
     new_sync_time = server_data.update_tab_list(client_data["tab_list"])
-    # 返回最近更新的时间点 由于使用客户端的数据, 因此无需返回
     return pack_result(SYNC_RESULT.SUCCESS, server_data.get_sync_time(), [])
